dbcb5e984ab7b2b8f453bee2de77dc8337a64ee6
Kan Dang <kandaaang@gmail.com>
2020-09-06 23:08:43 -0700

    Update app.js
    Implemented basic functions listed below:
    
    GET
    PUT
    POST
    DELETE
    
    tested using Postman

--- app.js
+++ app.js
@@ -7,7 +7,65 @@ app.use(bodyParser.json());
 const path = require('path')
 const db = require("./db")
 
-const collection = "todolist";
+const collection = "todoList";
+
+app.get('/', (req,res) => {
+
+    res.sendFile(paht.join(__dirname, 'index.html'));
+    
+})
+
+app.get('/getTodos', (req,res) => {
+    db.getDB().collection(collection).find({}).toArray((err,documents) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(documents);
+            res.json(documents);
+        }
+    })
+})
+
+
+app.put('/:id', (req,res) => {
+    const todoID = req.params.id;
+    const userInput = req.body;
+
+    db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(todoID)}, 
+        {$set : { todo : userInput.todo}}, 
+        {returnOriginal : false}, (err, result) => {
+            if(err) {
+                console.log(err);
+            } else {
+                res.json(result);
+            }
+        })
+})
+
+app.post('/', (req,res) => {
+    const userInput = req.body;
+    
+    db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.json({result  : result, document : result.ops[0]});
+        }
+    })
+
+})
+
+app.delete('/:id', (req,res) => {
+    const todoID = req.params.id;
+
+    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoID)}, (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    })
+})
 
 db.connect((err) => {
     if(err) {
